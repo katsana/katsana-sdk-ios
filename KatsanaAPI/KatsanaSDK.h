11c3c4e9827406d16aa00304c21192980636ce72
lutfime <lutfime_2000@yahoo.com>
2016-09-05 10:03:56 +0800

    Update config

--- KatsanaAPI/KatsanaSDK.h
+++ KatsanaAPI/KatsanaSDK.h
@@ -17,7 +17,16 @@ FOUNDATION_EXPORT const unsigned char KatsanaAPIVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <KatsanaAPI/PublicHeader.h>
 
 #import <RestKit/RestKit.h>
-//#import <KatsanaAPI/CDGeneratorParser.h>
-//#import <CodeGenerator/CDClassObject.h>
-//#import <CodeGenerator/CDCodeGenerator.h>
-//#import <CodeGenerator/CDMethodObject.h>
+#import <KatsanaSDK/KMUserManager.h>
+#import <KatsanaSDK/KMConstant.h>
+#import <KatsanaSDK/KMCacheManager.h>
+
+#import <KatsanaSDK/KMActivityObject.h>
+#import <KatsanaSDK/KMAddress.h>
+#import <KatsanaSDK/KMAvatar.h>
+#import <KatsanaSDK/KMTrip.h>
+#import <KatsanaSDK/KMUser.h>
+#import <KatsanaSDK/KMVehicle.h>
+#import <KatsanaSDK/KMVehicleDayHistory.h>
+#import <KatsanaSDK/KMVehiclePosition.h>
+#import <KatsanaSDK/KMViolation.h>
--- KatsanaAPITests/KatsanaAPITests.m
+++ KatsanaAPITests/KatsanaAPITests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <KatsanaSDK/KatsanaSDK.h>
 
 @interface KatsanaAPITests : XCTestCase
 
