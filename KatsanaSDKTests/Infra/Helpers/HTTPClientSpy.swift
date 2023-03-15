25b9c4a6f4121b84d40ae83f24911f53a5d427a1
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-15 10:22:27 +0800

    Move `makeUser` method to its own helper file

---
+++ KatsanaSDKTests/Infra/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,46 @@
+//
+//  HTTPClientSpy.swift
+//  KatsanaSDK
+//
+//  Created by Wan Ahmad Lutfi on 15/03/2023.
+//  Copyright © 2023 pixelated. All rights reserved.
+//
+
+import Foundation
+import KatsanaSDK
+
+class HTTPClientSpy: HTTPClient {
+    private struct Task: HTTPClientTask {
+        let callback: () -> Void
+        func cancel() { callback() }
+    }
+    
+    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+    private(set) var cancelledURLs = [URL]()
+    
+    var requestedURLs: [URL] {
+        return messages.map { $0.url }
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        messages.append((url, completion))
+        return Task { [weak self] in
+            self?.cancelledURLs.append(url)
+        }
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+    
+    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+        let response = HTTPURLResponse(
+            url: requestedURLs[index],
+            statusCode: code,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+        messages[index].completion(.success((data, response)))
+    }
+}
+
