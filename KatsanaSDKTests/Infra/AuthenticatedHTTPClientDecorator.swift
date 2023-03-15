8710355b9e5ced5a3f9be369c4024067e0024155
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-15 14:07:52 +0800

    Move `AuthenticatedHTTPClientDecorator` to prod

--- KatsanaSDKTests/Infra/AuthenticatedHTTPClientDecorator.swift
+++ KatsanaSDKTests/Infra/AuthenticatedHTTPClientDecorator.swift
@@ -9,54 +9,6 @@
 import XCTest
 import KatsanaSDK
 
-struct AccessToken{
-    let token: String
-}
-
-class AuthenticatedHTTPClientDecorator: HTTPClient{
-    let decoratee: HTTPClient
-    var token: AccessToken?
-    
-    public enum Error: Swift.Error {
-        case unauthorized
-    }
-    
-    class EmptyTask: HTTPClientTask{
-        func cancel() {}
-    }
-    
-    init(decoratee: HTTPClient) {
-        self.decoratee = decoratee
-    }
-    
-    func sign(_ accessToken: AccessToken){
-        self.token = accessToken
-    }
-    
-    func send(_ urlRequest: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        do{
-            let signedRequest = try self.signedRequest(for: urlRequest)
-            return decoratee.send(signedRequest, completion: completion)
-        }
-        catch{
-            completion(.failure(error))
-            return EmptyTask()
-        }
-    }
-    
-    func signedRequest(for request: URLRequest) throws -> URLRequest{
-        guard let token else {
-            throw Error.unauthorized
-        }
-        
-        var updatedRequest = request
-        updatedRequest.setValue("Bearer \(token.token)", forHTTPHeaderField: "Authorization ")
-        return request
-
-    }
-    
-}
-
 final class AuthenticatedHTTPClientDecoratorTests: XCTestCase {
     
     func test_sendRequest_withNoTokenReturnEmptyRequest(){
@@ -88,9 +40,5 @@ final class AuthenticatedHTTPClientDecoratorTests: XCTestCase {
     func testRequest() -> URLRequest{
         return URLRequest(url: URL(string: "/test")!)
     }
-    
-//    private struct TestRequest{
-//        var baseURL: URL { anyURL()}
-//        var path: String { "/test"}
-//    }
+
 }
