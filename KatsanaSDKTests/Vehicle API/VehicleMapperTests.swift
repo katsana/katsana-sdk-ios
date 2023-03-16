ee06df6240743d1878f3e5701b742012ad4600b3
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-16 11:34:41 +0800

    Move mapping `KTVehicle` to `VehicleMapper` class

--- KatsanaSDKTests/Vehicle API/VehicleMapperTests.swift
+++ KatsanaSDKTests/Vehicle API/VehicleMapperTests.swift
@@ -1,50 +1,41 @@
-////
-////  VehicleMapperTests.swift
-////  KatsanaSDKEndToEndTests
-////
-////  Created by Wan Ahmad Lutfi on 16/03/2023.
-////  Copyright © 2023 pixelated. All rights reserved.
-////
 //
-//import XCTest
-//import KatsanaSDK
+//  VehicleMapperTests.swift
+//  KatsanaSDKEndToEndTests
 //
-//class UserMapperMapperTests: XCTestCase {
-//    
-//    func test_map_throwsErrorOnNon200HTTPResponse() throws {
-//        let json = makeJSON([:])
-//        let samples = [199, 201, 300, 400, 500]
+//  Created by Wan Ahmad Lutfi on 16/03/2023.
+//  Copyright © 2023 pixelated. All rights reserved.
+//
+
+import XCTest
+import KatsanaSDK
+
+class VehicleMapperMapperTests: XCTestCase {
+
+    func test_map_deliversItemsJSONItems() throws {
+        let fleet = Fleet(fleetId: 2, name: "Main Fleet", deviceCount: 10)
+        let fleet2 = Fleet(fleetId: 6, name: "Secondary Fleet", deviceCount: 2)
+        
+        let (item, json) = makeUser(id: 220, email: "developer-demo@katsana.com", fleets: [fleet, fleet2])
+        let data = makeJSON(json)
+
+        let result = try UserMapper.map(data, from: HTTPURLResponse(statusCode: 200))
+
+        XCTAssertEqual(result, item)
+    }
+    
+    // MARK:  Helper
+    
+//    func makeVehicle(id: Int,
+//                     imei: String,
+//                     vehicleNumber: String,
+//                          imageURL: String = "null",
+//                          plan: UserPlan? = nil,
+//                          fleets: [Fleet] = [],
+//                          createdAt: String = "2019-11-05 04:47:52",
+//                  updatedAt: String? = "2019-11-05 04:47:52") -> (model: KTUser, json: [String: Any]) {
 //        
-//        try samples.forEach { code in
-//            XCTAssertThrowsError(
-//                try UserMapper.map(json, from: HTTPURLResponse(statusCode: code))
-//            )
-//        }
-//    }
-//    
-//    func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
-//        let invalidJSON = Data("invalid json".utf8)
-//
-//        XCTAssertThrowsError(
-//            try UserMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: 200))
-//        )
-//    }
-//
-//
-//    func test_map_deliversItemsOn200HTTPResponseWithJSONItems() throws {
-//        let fleet = Fleet(fleetId: 2, name: "Main Fleet", deviceCount: 10)
-//        let fleet2 = Fleet(fleetId: 6, name: "Secondary Fleet", deviceCount: 2)
+//        let item = KTUser(userId: "\(id)", email: email, imageURL: imageURL, plan: plan, company: nil, fleets: fleets, createdAt: createdAt.date(gmt: 0)!, updatedAt: updatedAt?.date(gmt: 0))
 //        
-//        let (item, json) = makeUser(id: 220, email: "developer-demo@katsana.com", fleets: [fleet, fleet2])
-//        let data = makeJSON(json)
-//
-//        let result = try UserMapper.map(data, from: HTTPURLResponse(statusCode: 200))
-//
-//        XCTAssertEqual(result, item)
 //    }
-//    
-//    // MARK: - Helpers
-//        
-//
-//    
-//}
+    
+}
