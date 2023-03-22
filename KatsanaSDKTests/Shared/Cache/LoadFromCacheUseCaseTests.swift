a7446a552a98069d6ed932e77530f23601a182c3
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-23 06:42:17 +0800

    Load cache fails on retrieval error

--- KatsanaSDKTests/Shared/Cache/LoadFromCacheUseCaseTests.swift
+++ KatsanaSDKTests/Shared/Cache/LoadFromCacheUseCaseTests.swift
@@ -48,6 +48,10 @@ class FeedStoreSpy<R>: FeedStore where R: Equatable{
 
     private(set) var receivedMessages = [ReceivedMessage]()
     
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    private var retrievalCompletions = [RetrievalCompletion]()
+    
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
     }
@@ -57,7 +61,12 @@ class FeedStoreSpy<R>: FeedStore where R: Equatable{
     }
     
     func retrieve(completion: @escaping RetrievalCompletion) {
+        retrievalCompletions.append(completion)
         receivedMessages.append(.retrieve)
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrievalCompletions[index](.failure(error))
     }
 }
 
@@ -74,18 +83,19 @@ public final class LocalLoader<Resource, FeedStoreType: FeedStore> where Resourc
 
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
-//            guard let self = self else { return }
-//
-//            switch result {
-//            case let .failure(error):
-//                completion(.failure(error))
-//
+            guard let self = self else { return }
+
+            switch result {
+            case let .failure(error):
+                completion(.failure(error))
+            default:
+                ()
 //            case let .success(.some(cache)): // where FeedCachePolicy.validate(cache.timestamp, against: self.currentDate()):
 //                completion(.success(cache.feed.toModels()))
-
+//
 //            case .success:
 //                completion(.success([]))
-//            }
+            }
         }
     }
 }
@@ -93,23 +103,32 @@ public final class LocalLoader<Resource, FeedStoreType: FeedStore> where Resourc
 class LoadFromCacheUseCaseTests: XCTestCase {
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
-
+        
         XCTAssertEqual(store.receivedMessages, [])
     }
     
     func test_load_requestsCacheRetrieval() {
         let (sut, store) = makeSUT()
-
+        
         sut.load { _ in }
-
+        
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failsOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        let retrievalError = anyNSError()
+        
+        expect(sut, toCompleteWith: .failure(retrievalError), when: {
+            store.completeRetrieval(with: retrievalError)
+        })
     }
     
     // MARK: - Helpers
     
     typealias FeedStoreSpyType = FeedStoreSpy<String>
     typealias LocalLoaderType = LocalLoader<String, FeedStoreSpyType>
-
+    
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalLoaderType, store: FeedStoreSpyType) {
         let store = FeedStoreSpyType()
         let sut = LocalLoaderType(store: store, currentDate: currentDate)
@@ -117,4 +136,27 @@ class LoadFromCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
+    
+    private func expect(_ sut: LocalLoaderType, toCompleteWith expectedResult: LocalLoaderType.LoadResult, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedImages), .success(expectedImages)):
+                XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+
+            default:
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+
+        action()
+        wait(for: [exp], timeout: 1.0)
+    }
+    
 }
