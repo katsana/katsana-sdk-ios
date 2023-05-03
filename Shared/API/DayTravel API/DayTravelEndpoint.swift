4949ea8a974de5816006804feffb8e8987ef133b
Wan Lutfi <lutfime_2000@yahoo.com>
2023-05-03 10:45:26 +0800

    Added DayTravelEndpoint

---
+++ Shared/API/DayTravel API/DayTravelEndpoint.swift
@@ -0,0 +1,27 @@
+//
+//  DayTravelEndpoint.swift
+//  KatsanaSDK
+//
+//  Created by Wan Ahmad Lutfi on 03/05/2023.
+//  Copyright © 2023 pixelated. All rights reserved.
+//
+
+import Foundation
+
+public enum DayTravelEndpoint {
+    case get(vehicleId: Int, date: Date)
+
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case let .get(vehicleId, date):
+            var queryItems: [URLQueryItem]?
+//            queryItems = [URLQueryItem(name: "start", value: date.toStringWithYearMonthDay())]
+            
+            let urlString = baseURL.absoluteString + "/vehicles" + "/\(vehicleId)" + "/travels/ " + date.toStringWithYearMonthDay()
+            let url = URL(string: urlString)!
+            
+            let finalURL = URL.make(url: url, queryItems: queryItems)
+            return finalURL!
+        }
+    }
+}
--- Shared/API/Trips API/TripEndpoint.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TripEndpoint.swift
-//  KatsanaSDK
-//
-//  Created by Wan Ahmad Lutfi on 03/05/2023.
-//  Copyright © 2023 pixelated. All rights reserved.
-//
-
-import Foundation
