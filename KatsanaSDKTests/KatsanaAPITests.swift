741d773995005de9f2f300784cf81830ca1fd026
Wan Lutfi <lutfime_2000@yahoo.com>
2023-07-05 09:33:11 +0800

    Ensure API does not authenticate when login failure

--- KatsanaSDKTests/KatsanaAPITests.swift
+++ KatsanaSDKTests/KatsanaAPITests.swift
@@ -10,10 +10,19 @@ import XCTest
 import KatsanaSDK
 
 class KatsanaAPI{
+    let tokenService: TokenService
+    
     var isAuthenticated = false
     
-    func login(email: String, password: String){
-        isAuthenticated = true
+    init(tokenService: TokenService) {
+        self.tokenService = tokenService
+    }
+    
+    func login(email: String, password: String, completion: @escaping (TokenService.Result) -> Void){
+        tokenService.getToken {[weak self] result in
+            self?.isAuthenticated = ((try? result.get()) != nil)
+            completion(result)
+        }
     }
 }
 
@@ -21,18 +30,44 @@ final class KatsanaAPITests: XCTestCase {
     
     func test_init_isNotAuthenticated() {
         let sut = makeSUT()
-        
-        XCTAssertTrue(!sut.isAuthenticated)
+        XCTAssertFalse(sut.isAuthenticated)
     }
     
-    func test_login_authenticateWhenSuccess() {
+    func test_login_changeToAuthenticatedWhenSuccess() {
         let sut = makeSUT()
-        sut.login(email: "test", password: "1212")
+        
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.login(email: "test", password: "1212") { result in
+            if let _ = try? result.get(){
+                //Do nothing
+            }else{
+                XCTFail("Expected to success")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
         XCTAssertTrue(sut.isAuthenticated)
     }
     
-    func makeSUT() -> KatsanaAPI{
-        let sut = KatsanaAPI()
+    func test_login_doesNotAuthenticateWhenFailure() {
+        let tokenStub = TokenServiceStub(stub: .failure(anyNSError()))
+        let sut = makeSUT(tokenService: tokenStub)
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.login(email: "test", password: "wrongPass") { result in
+            if let _ = try? result.get(){
+                XCTFail("Expected to fail")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertFalse(sut.isAuthenticated)
+    }
+    
+    func makeSUT(tokenService: TokenService? = nil) -> KatsanaAPI{
+        let theTokenService = tokenService ?? TokenServiceStub(stub: .success(AccessToken(token: "any")))
+        let sut = KatsanaAPI(tokenService: theTokenService)
         return sut
     }
     
