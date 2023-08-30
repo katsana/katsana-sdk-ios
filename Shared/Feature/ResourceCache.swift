952b88e546efc642b38d9db2db7b471802b5b041
Wan Lutfi <lutfime_2000@yahoo.com>
2023-08-30 20:59:31 +0800

    Make FirebaseNotificationSettingsLoader confirm to ResourceCache so we can save notification profile to firebase

--- Shared/Feature/ResourceCache.swift
+++ Shared/Feature/ResourceCache.swift
@@ -14,3 +14,23 @@ public protocol ResourceCache {
 
     func save(_ resource: SaveResource, completion: @escaping (SaveResult) -> Void)
 }
+
+public class AnyResourceCache<Resource>: ResourceCache {
+    private let saverObject:  (Resource, @escaping (Result<Void, Error>) -> Void) -> ()
+
+    public init<L: ResourceCache>(wrappedLoader: L) where L.SaveResource == Resource {
+        self.saverObject = wrappedLoader.save
+    }
+    
+    public func save(_ resource: Resource, completion: @escaping (Result<Void, Error>) -> Void) {
+        saverObject(resource, completion)
+    }
+}
+
+public extension ResourceCache{
+    func eraseToAnyResourceCache() -> AnyResourceCache<SaveResource>{
+        return AnyResourceCache(wrappedLoader: self)
+    }
+}
+
+
